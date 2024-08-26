e42277fd5b166b5c91717e0b51b8af38c448305c
anhkiet12311 <leanhkiet114477@gmail.com>
2024-08-26 16:42:20 +0700

    botapp12

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -29,17 +29,19 @@ export default function Home() {
         (
           <>
             <h1 className="text-2xl font-bold mb-4 ">User Data </h1>
-            <ul>
-              <li className="font-bold text-green-500">ID: {UserData.id}</li>
-              <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-              <li className="font-bold text-purple-500">Last Nam: {UserData.last_name}</li>
-              <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-              <li className="font-bold text-orange-500">Username: {UserData.username}</li>
-              <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-              <li className="font-bold text-teal-500">Language code: {UserData.languge_code}</li>
-              <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-              <li className="font-bold text-yellow-500">is Premium: {UserData.is_premium ? 'Yes' : 'No'}</li>
-            </ul>
+            <div className="bg-gray-500 bg-opacity-50 backdrop-blur-md p-6 rounded-lg">
+              <ul>
+                <li className="font-bold text-green-500">ID: {UserData.id}</li>
+                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
+                <li className="font-bold text-purple-500">Last Nam: {UserData.last_name}</li>
+                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
+                <li className="font-bold text-orange-500">Username: {UserData.username}</li>
+                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
+                <li className="font-bold text-teal-500">Language code: {UserData.languge_code}</li>
+                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
+                <li className="font-bold text-yellow-500">is Premium: {UserData.is_premium ? 'Yes' : 'No'}</li>
+              </ul>
+            </div>
           </>
         ):
         (
