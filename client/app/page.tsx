21f1c6a22aebf0b6605328222a02b778cf2193cc
anhkiet12311 <leanhkiet114477@gmail.com>
2024-08-26 17:04:10 +0700

    botapp32

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -28,18 +28,15 @@ export default function Home() {
         UserData ?
         (
           <>
-            <h1 className="text-2xl font-bold mb-4 ">User Data </h1>
-            <div className="bg-gray-500 bg-opacity-50 backdrop-blur-md p-6 rounded-lg">
+            <h1 className="text-xl font-bold mb-4 "> Your Data </h1>
+            <div className="bg-gray-300 bg-opacity-50 backdrop-blur-md p-6 rounded-lg">
               <ul>
-                <li className="font-bold text-green-500">ID: {UserData.id}</li>
-                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-                <li className="font-bold text-purple-500">Last Nam: {UserData.last_name}</li>
-                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-                <li className="font-bold text-orange-500">Username: {UserData.username}</li>
-                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-                <li className="font-bold text-teal-500">Language code: {UserData.languge_code}</li>
-                <li className="font-bold text-red-500">First Name: {UserData.first_name}</li>
-                <li className="font-bold text-yellow-500">is Premium: {UserData.is_premium ? 'Yes' : 'No'}</li>
+                <li className="font-bold text-black">ID: {UserData.id}</li>
+                <li className="font-bold text-black">First Name: {UserData.first_name || "N/A"}</li>
+                <li className="font-bold text-black">Last Name: {UserData.last_name || "N/A"}</li>
+                <li className="font-bold text-black">Username: {UserData.username}</li>
+                <li className="font-bold text-black">Language code: {UserData.languge_code || "N/A"}</li>
+                <li className="font-bold text-black">is Premium: {UserData.is_premium ? 'Yes' : 'No'}</li>
               </ul>
             </div>
           </>
